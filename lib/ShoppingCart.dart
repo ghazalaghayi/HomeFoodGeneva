47c6d0f6e5657a52703946c98fbb20ab4b248143
Ghazal <ghazal.aghayi@yahoo.com>
2021-08-02 08:54:12 +0430

    Shopping Cart Added !

--- lib/ShoppingCart.dart
+++ lib/ShoppingCart.dart
@@ -1,119 +1,64 @@
-import 'package:flutter/material.dart';
-import 'package:home_food_geneva/NavigationBar.dart';
+import 'item.dart';
+import 'package:uuid/uuid.dart';
 
-class Product {
-  const Product(this.price, {@required this.name});
-  final String name;
-  final double price;
-}
-
-typedef void CartChangedCallback(Product product, bool inCart);
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({
-    @required this.product,
-    @required this.inCart,
-    @required this.onCartChanged,
-  }) : super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context) {
-    // The theme depends on the BuildContext because different
-    // parts of the tree can have different themes.
-    // The BuildContext indicates where the build is
-    // taking place and therefore which theme to use.
-
-    return inCart //
-        ? Colors.white
-        : Theme.of(context).primaryColor;
-  }
-
-  TextStyle _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return TextStyle(
-      color: Colors.white,
-      fontFamily: "OpenSans",
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(product.name, style: _getTextStyle(context)),
-    );
-  }
-}
-
-class ShoppingList extends StatefulWidget {
-  ShoppingList({key, @required this.products}) : super(key: key);
-
-  final List<Product> products;
-
-  // The framework calls createState the first time
-  // a widget appears at a given location in the tree.
-  // If the parent rebuilds and uses the same type of
-  // widget (with the same key), the framework re-uses
-  // the State object instead of creating a new State object.
-
-  @override
-  _ShoppingListState createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  Set<Product> _shoppingCart = Set<Product>();
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {
-      // When a user changes what's in the cart, you need
-      // to change _shoppingCart inside a setState call to
-      // trigger a rebuild.
-      // The framework then calls build, below,
-      // which updates the visual appearance of the app.
-
-      if (!inCart)
-        _shoppingCart.add(product);
-      else
-        _shoppingCart.remove(product);
+class ShoppingCart {
+  final orderId = Uuid().v4();
+  List<Item> items = [];
+  bool get isEmpty => items.isEmpty;
+  int get numOfItems => items.length;
+  int get totalPrice {
+    int totalPrice = 0;
+    items.forEach((i) {
+      totalPrice += i.price;
     });
+    return totalPrice;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping Cart'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 8.0),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[400],
-        onPressed: () {},
-        child: Icon(
-          Icons.done,
-          color: Colors.black,
-        ),
-      ),
-      bottomNavigationBar: NavigationBar(),
-    );
+  String get formattedTotalPrice {
+    if (isEmpty) {
+      return Item.formatter.format(0);
+    }
+    return Item.formatter.format(this.totalPrice);
+  }
+
+  bool isExists(item) {
+    if (items.isEmpty) {
+      return false;
+    }
+    final indexOfItem = items.indexWhere((i) => item.id == i.id);
+    return indexOfItem >= 0;
+  }
+
+  void add(Item item) {
+    if (items.isEmpty) {
+      items.add(item);
+      return;
+    }
+    if (!this.isExists(item)) {
+      items.add(item);
+    }
+  }
+
+  void remove(Item item) {
+    if (items.isEmpty) return;
+    final indexOfItem = items.indexWhere((i) => item.id == i.id);
+    if (indexOfItem >= 0) {
+      items.removeAt(indexOfItem);
+    }
+  }
+
+  Map<String, dynamic> get toMap {
+    final List<Map<String, dynamic>> items = this
+        .items
+        .map((i) => {
+              'id': i.id,
+              'name': i.name,
+              'description': i.description,
+              'price': i.price,
+              'inStock': i.inStock,
+              'imageUrl': i.imageUrl
+            })
+        .toList();
+    return {"orderId": this.orderId, "items": items, "total": this.totalPrice};
   }
 }
